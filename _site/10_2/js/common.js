a846363e1625bd6652bdcc0195b94f9fb0bad4fb
ssus-dev <ssu.private@gmail.com>
2023-06-05 15:33:28 +0900

    theme fixed

--- _site/10_2/js/common.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    // tab menu
-    $('.slide_tab > li').click(function (e) { 
-        e.preventDefault();
-        $('.slide_tab > li').not($(this).addClass('active')).removeClass('active');
-    });
-
-
-})
--- _site/README.md
+++
@@ -1,1 +0,0 @@
-# ssus-dev.github.io
